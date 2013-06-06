e3731e3464397d6f763dc69fd938a3f75ac17cbd
Sami Jaber <jabersami@gmail.com>
2013-06-06 16:53:41 -0400

    Now keeps track of Turtle API line locations

--- src/main/scala/net/kogics/kojo/lite/Tracing.scala
+++ src/main/scala/net/kogics/kojo/lite/Tracing.scala
@@ -1,147 +1,169 @@
 package net.kogics.kojo.lite
 
-import scala.util.control.Breaks._
-import scala.collection.JavaConversions._
-import com.sun.jdi.connect._
-import com.sun.jdi.event.MethodExitEvent
-import com.sun.jdi.request._
+import java.io.File
+
+import scala.collection.JavaConversions.asScalaBuffer
+import scala.collection.JavaConversions.asScalaIterator
+import scala.reflect.internal.util.BatchSourceFile
+import scala.tools.nsc.Global
+import scala.tools.nsc.Settings
+import scala.util.control.Breaks.break
+import scala.util.control.Breaks.breakable
+
 import com.sun.jdi.Bootstrap
-import com.sun.jdi.StringReference
 import com.sun.jdi.ThreadReference
 import com.sun.jdi.VirtualMachine
-import com.sun.jdi.event.VMDisconnectEvent
-import com.sun.jdi.event.EventQueue
+import com.sun.jdi.connect.Connector
+import com.sun.jdi.connect.LaunchingConnector
 import com.sun.jdi.event.EventSet
 import com.sun.jdi.event.MethodEntryEvent
-import java.io._
-import scala.tools.nsc.Settings
-import scala.tools.nsc.Global
-import scala.tools.nsc.reporters._
+import com.sun.jdi.event.MethodExitEvent
+import com.sun.jdi.event.VMDisconnectEvent
+import com.sun.jdi.request.EventRequest
 
-import scala.reflect.runtime._
-import scala.reflect.internal.util._
-import scala.tools.reflect.ToolBox
+import net.kogics.kojo.util.Utils
 
 class Tracing {
-  
+
   var evtSet: EventSet = _
   var initconn: LaunchingConnector = _
   var mainThread: ThreadReference = _
   var codeFile: BatchSourceFile = _
   val tmpdir = System.getProperty("java.io.tmpdir")
-  val settings = makeSettings()  
-  val compiler = new Global(settings) 
+  val settings = makeSettings()
+  val compiler = new Global(settings)
+
+  val wrapperCode = """object Wrapper { 
+  def main(args: Array[String]) { 
+    %s
+  }
+    
+  def clear() {}
+  def forward(n: Double) {}
+  def right(n: Double) {}
+  def repeat(n: Int) (fn: => Unit) {
+    var i = 0
+    while(i < n) {
+      fn
+      i += 1
+    }
+  }
+} 
+"""
+
+  def compile(code0: String) = {
+    val code = wrapperCode format code0
+    codeFile = new BatchSourceFile("scripteditor", code)
+    val run = new compiler.Run
+    run.compileSources(List(codeFile))
+    
+  }
 
   def makeSettings() = {
-      val iSettings = new Settings()
-      iSettings.usejavacp.value = true
-      iSettings.outputDirs.setSingleOutput(tmpdir)
-      iSettings
-    }
+    val iSettings = new Settings()
+    iSettings.usejavacp.value = true
+    iSettings.outputDirs.setSingleOutput(tmpdir)
+    iSettings
+  }
 
-  def compile(code0: String, stopPhase: List[String] = List("cleanup")) = {
-	val code = "object Wrapper { def main(args: Array[String]) { } \n" + code0 + "}"
-    codeFile = new BatchSourceFile("scripteditor", code)
-	val run = new compiler.Run
-    run.compileSources(List(codeFile))
-  }  
-
- 
-  def getVM(initconn : LaunchingConnector) = {
+  def getVM(initconn: LaunchingConnector) = {
     var connector = initconn
 
     val conns = Bootstrap.virtualMachineManager().allConnectors();
-    breakable { 
-      for (conn <- conns) { 
-      if (conn.name().equals("com.sun.jdi.CommandLineLaunch"))
-        {
+    breakable {
+      for (conn <- conns) {
+        if (conn.name().equals("com.sun.jdi.CommandLineLaunch")) {
           connector = conn.asInstanceOf[LaunchingConnector]
           break
         }
       }
     }
 
-    // get connector field for program's main() method
+    // set connector arguments
     val connArgs = connector.defaultArguments();
     val mArgs = connArgs.get("main").asInstanceOf[Connector.Argument]
-    if (mArgs == null)
-     throw new Error("Bad launching connector");
-    // concatenate all tracer's input args into a single string
-    val sb = new StringBuffer();
-    sb.append(" Wrapper ")
-    mArgs.setValue(sb.toString()); // assign args to main field
-    
     val opts = connArgs.get("options").asInstanceOf[Connector.Argument]
-    var optionValue = "-classpath " + tmpdir
-	opts.setValue(optionValue)
+    var optionValue = s"-classpath $tmpdir${File.pathSeparator}/tmp/scala-library.jar"
+
+    if (mArgs == null)
+      throw new Error("Bad launching connector");
+
+    mArgs.setValue("Wrapper"); // assign args to main field
+    opts.setValue(optionValue) //assign args to options field
 
     val vm = connector.launch(connArgs)
     vm
   }
+
+  def trace(code: String) = Utils.runAsync {
  
- /* Printing to file is unnecessary now (No need for .scala file, only compiled .class file is needed for tracing)
-  def printToFile(f: java.io.File)(op: java.io.PrintWriter => Unit) {
-  val p = new java.io.PrintWriter(f)
-  try { op(p) } finally { p.close() }
-}
-  
-def createWrapper(code: String) {
-  val data = Array("object Wrapper { \n def main(args: Array[String]) {} \n", code, "}")
-  printToFile(new File("Wrapper.scala"))(p => {
-	  data.foreach(p.println)
-  })
-}
-  */
-  
-def trace(code: String){
-  //create the file
-  //createWrapper(code)
-  
-  val result = compile(code, Nil)
-  //Connect to target VM
-  val vm = getVM(initconn)
-  println("Attached to process '" + vm.name + "'")
+    val result = compile(code)
+    //Connect to target VM
+    val vm = getVM(initconn)
+    println("Attached to process '" + vm.name + "'")
 
+    //Create Event Requests
+    val excludes = Array("java.*", "javax.*", "sun.*", "com.sun.*", "com.apple.*")
+    createRequests(excludes, vm);
 
-  //Create Event Requests
-  val excludes = Array("java.*", "javax.*", "sun.*", "com.sun.*", "com.apple.*")
-  createRequests(excludes, vm);
+    //Iterate through Events
+    val evtQueue = vm.eventQueue
+    vm.resume
 
-  //Iterate through Events
-  val evtQueue = vm.eventQueue
-  vm.resume
+    //Find main thread in target VM
+    val allThrds = vm.allThreads
+    allThrds.foreach { x => if (x.name == "main") mainThread = x }
 
-  //Find main thread in target VM
-  val allThrds = vm.allThreads
-  allThrds.foreach { x => if (x.name == "main") mainThread = x}
-  
-  breakable { while (true) {
-    evtSet = evtQueue.remove()
-	for (evt <- evtSet.eventIterator) {
-		evt match {
-		case methodEnterEvt: MethodEntryEvent => 
-		try {
-			//Locate current stackframe, find get value of 'n' variable
-			val frame = mainThread.frame(0)
-					val n = methodEnterEvt.method().arguments()(0)
-					val argval = frame.getValue(n)
-					println("Method Enter Event (arg n): " + argval)
-		}
-		catch {
-		case _: Throwable => println("Method Enter Event: " + methodEnterEvt.method().name)
-		}
-		case methodExitEvt: MethodExitEvent   => println("Method Exit Event (return value): " + methodExitEvt.returnValue)
-		case vmDcEvt: VMDisconnectEvent		  => println("VM Disconnected"); break
-		case _                                => println("Other")
-		}
-	}
-	evtSet.resume()
+    breakable {
+      while (true) {
+        evtSet = evtQueue.remove()
+        for (evt <- evtSet.eventIterator) {
+          evt match {
+            case methodEnterEvt: MethodEntryEvent =>
+              try {
+                //Locate current stackframe, find get value of 'n' variable
+                val frame = mainThread.frame(0)
+                var toprint = "";
+                
+                if(methodEnterEvt.method().arguments().size > 0){
+                	var n = methodEnterEvt.method().arguments()(0)
+               		var argval = frame.getValue(n)
+                	var argname = n.name
+                	toprint = " (arg " + n.name + "): " + argval
+                 }
+                    
+                //determine if the method is a Turtle API method
+                methodEnterEvt.method().name match {
+                  case "forward" | "right" | "clear" =>                 
+                    println(s"Method Enter Event [${mainThread.frame(1).location().lineNumber - 2}] ${methodEnterEvt.method().name}" + toprint)
+                  case _ =>
+                    println(s"Method Enter Event [${methodEnterEvt.location().lineNumber - 2}] ${methodEnterEvt.method().name}" + toprint)
+                }
+              }
+              catch {
+                case _: Throwable => println(s"Method Enter Event [${methodEnterEvt.location().lineNumber - 2}]  " + methodEnterEvt.method().name)
+              }
+            case methodExitEvt: MethodExitEvent => 
+              //determine if the method is a Turtle API method
+                methodExitEvt.method().name match {
+                  case "forward" | "right" | "clear" =>                 
+                    println(s"Method Exit Event [${mainThread.frame(1).location().lineNumber - 2}] ${methodExitEvt.method().name}(return value): " + methodExitEvt.returnValue)                  
+                  case _ =>
+                    println(s"Method Exit Event [${methodExitEvt.location().lineNumber - 2}] ${methodExitEvt.method().name}(return value): " + methodExitEvt.returnValue)                
+               }
+            case vmDcEvt: VMDisconnectEvent =>
+              println("VM Disconnected"); break
+            case _ => println("Other")
+          }
+        }
+        evtSet.resume()
+      }
+    }
   }
-}}
 
-  def createRequests(excludes : Array[String], vm: VirtualMachine) {
+  def createRequests(excludes: Array[String], vm: VirtualMachine) {
     val evtReqMgr = vm.eventRequestManager
-    
+
     val mthdEnterVal = evtReqMgr.createMethodEntryRequest()
     excludes.foreach { mthdEnterVal.addClassExclusionFilter(_) }
     mthdEnterVal.setSuspendPolicy(EventRequest.SUSPEND_EVENT_THREAD)
