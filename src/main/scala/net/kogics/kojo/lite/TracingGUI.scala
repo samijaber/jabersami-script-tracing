66ee0eca522be11bf8c7df48689c1cebb2bf0293
Sami Jaber <jabersami@gmail.com>
2013-06-22 14:06:18 -0500

    Implemented an event class, and provide proper indents in GUI.

--- src/main/scala/net/kogics/kojo/lite/TracingGUI.scala
+++ src/main/scala/net/kogics/kojo/lite/TracingGUI.scala
@@ -4,33 +4,63 @@ import swing._
 import com.sun.jdi.ThreadReference
 import com.sun.jdi.StackFrame
 
+class event {
+  var ended = false
+  var entry: String = _
+  var exit: String = _
+  var subcalls = new Array[event](0)
+  var parent: event = null
+  
+  def isOver() = ended = true
+  def addChild(c : event) = {c.setParent(this); subcalls :+ c}
+  def setParent(p: event) = parent = p
+}
 
 object TracingGUI extends SimpleSwingApplication {
   var box: BoxPanel = _
-  var counter: Int = 0
-  var stack: Array[StackFrame] = new Array[StackFrame](0)
+  var main = new event()
+  var events = main.subcalls
+  var lastEvent = main
   
   def top = new MainFrame {
     title = "Tracing Stack"
     box = new BoxPanel(Orientation.Vertical)
     contents = box
-    counter = 0
-    stack = new Array[StackFrame](0)
   }
   
-  def addEvent(txt: String, prevframe: StackFrame, currframe: StackFrame) {
-    counter = counter + 1
+  def getLength(evt: event): Int = evt.parent match {
+    case null => 0
+    case _ => getLength(evt.parent) + 1 
+  }
+  
+  def addEvent(prompt: String, evt: String, isTurtle: Boolean) {
+    evt match {
+      case "entry" => 
+        //create new event
+        var newEvt = new event()
+        newEvt.entry = prompt
+        
+        //set parent of new event
+        lastEvent.ended match {
+          case true  => newEvt.parent = lastEvent.parent
+          case false => newEvt.parent = lastEvent
+        }
+        lastEvent = newEvt
+        
+      case "exit" =>
+        if (lastEvent.ended)
+          {lastEvent = lastEvent.parent}  
+        lastEvent.isOver()
+        lastEvent.exit = prompt
+    }    
     
     box.contents += new TextField{
-      text = "Event #" + counter + ":" + txt
-      try { if (stack.length > 1 && stack.indexOf(prevframe) != -1)
-      		{text += ". Also, this event was called from within the call at event #" + stack.indexOf(prevframe)}} 
-      try { if (stack.length > 1 && stack.indexOf(currframe) != -1)
-      		{text += ". Also, this event is occuring on the same level as event #" + stack.indexOf(currframe)}} 
+      for (i <- 1 to getLength(lastEvent))
+        text += "#"
+      text += prompt 
       editable = false
       }
 
-    stack :+ currframe
     box.visible = false
     box.visible = true
   } 
